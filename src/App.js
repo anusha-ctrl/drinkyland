dee88608cdcf23a52036af46ea5f503ddf39541b
devanshk <dkukreja@fb.com>
2020-04-12 14:10:18 -0700

    added firebase sync example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js'; // <--- add this line
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-logo">DrinkyLand</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {playerVal: 'local value here so far'}
+  }
+
+  componentDidMount() {
+    const playersRef = firebase.database().ref('players');
+    playersRef.on('value', (snapshot) => {
+      let playerVal = snapshot.val();
+      this.setState({
+        players: playerVal
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-logo">DrinkyLand</h1>
+          <p>{this.state.players}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
