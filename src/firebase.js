298750b4ba025c01857aa5464f1183a4fcb3c482
devanshk <dkukreja@fb.com>
2020-04-12 13:59:50 -0700

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrq3TG-U0VQbdteIHgfNxA3Sa-QIQ-Sxo",
+  authDomain: "drinkyland-22e88.firebaseapp.com",
+  databaseURL: "https://drinkyland-22e88.firebaseio.com",
+  projectId: "drinkyland-22e88",
+  storageBucket: "drinkyland-22e88.appspot.com",
+  messagingSenderId: "844746822305",
+  appId: "1:844746822305:web:0261781f7b2b41802cd6e0",
+  measurementId: "G-T6ZVB3LWMG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
